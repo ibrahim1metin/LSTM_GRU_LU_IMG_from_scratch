f328221ff1f4fb9c0ceaa4f6524fe5862d1911ac
ibrahim1metin <107308009+ibrahim1metin@users.noreply.github.com>
2022-11-11 22:47:25 +0300

    Update LU.py

--- LU.py
+++ LU.py
@@ -1,26 +1,29 @@
 import tensorflow as tf
-class LU(tf.keras.layers.Layer):
-    def __init__(self,hid_nodes,**kwargs):
-        super(LU,self).__init__(dynamic=True)
-        self.hid_nodes=hid_nodes
-        self.state_size=hid_nodes
-        self.output_size=hid_nodes
+class GRU(tf.keras.layers.Layer):
+    def __init__(self,hid_nod,**kwargs):
+      super(GRU,self).__init__(dynamic=False)
+      self.hidden_nodes=hid_nod
+      self.state_size=[tf.TensorShape((hid_nod,)),tf.TensorShape((hid_nod,))]
+      self.output_size=hid_nod
     def build(self,i):
-        self.ux=self.add_weight(shape=(self.hid_nodes,self.hid_nodes),trainable=True)
-        self.wx=self.add_weight(shape=(i[-1],self.hid_nodes),trainable=True)
-        self.wx2=self.add_weight(shape=(i[-1],self.hid_nodes),trainable=True)
-        self.bx2=self.add_weight(shape=(self.hid_nodes,),trainable=True)
-        self.bx=self.add_weight(shape=(self.hid_nodes,),trainable=True)
-        self.uh=self.add_weight(shape=(self.hid_nodes,self.hid_nodes),trainable=True)
-        self.built=True
-    def call(self,input,state):
-        state=state[0]
-        input=input
-        u=tf.math.sigmoid(tf.matmul(input,self.wx)+tf.matmul(state,self.ux)+self.bx)
-        h_=tf.math.tanh(tf.matmul(input,self.wx2)+self.bx2+tf.matmul(state,self.uh))
-        h=tf.multiply((1-u),state)+tf.multiply(u,h_)
-        return h,[h]
+      self.wx=self.add_weight(shape=(i[-1],self.hidden_nodes),trainable=True)
+      self.bx=self.add_weight(shape=(self.hidden_nodes,),trainable=True)
+      self.wh=self.add_weight(shape=(self.hidden_nodes,self.hidden_nodes),trainable=True)
+      self.wr=self.add_weight(shape=(i[-1],self.hidden_nodes),trainable=True)
+      self.ur=self.add_weight(shape=(self.hidden_nodes,self.hidden_nodes),trainable=True)
+      self.uz=self.add_weight(shape=(self.hidden_nodes,self.hidden_nodes),trainable=True)
+      self.wz=self.add_weight(shape=(i[-1],self.hidden_nodes),trainable=True)
+      self.br=self.add_weight(shape=(self.hidden_nodes,),trainable=True)
+      self.bz=self.add_weight(shape=(self.hidden_nodes,),trainable=True)
+      self.built=True
+    def call(self,inp,state):
+      state=state[0]
+      z=tf.math.sigmoid(tf.linalg.matmul(inp,self.wz)+tf.linalg.matmul(state,self.uz)+self.bz)
+      r=tf.math.sigmoid(tf.linalg.matmul(inp,self.wr)+tf.linalg.matmul(state,self.ur)+self.br)
+      h_=tf.math.tanh(tf.linalg.matmul(inp,self.wx)+self.bx+tf.linalg.matmul(state,self.wh)*r)
+      hid_state=tf.math.multiply((1-z),h_)+tf.math.multiply(state,z)
+      return hid_state,[hid_state]
     def get_config(self):
-        config=super().get_config()
-        config.update({"hid_nodes":self.hid_nodes})
-        return config
+      config=super().get_config()
+      config.update({"hid_nod":self.hidden_nodes})
+      return config
